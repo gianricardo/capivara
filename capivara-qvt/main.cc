5a42611e7e4521ef8ccd3424527ed41b00548b94
gian <gian.ricardo@gmail.com>
2018-09-19 18:43:52 -0300

    begin

---
+++ capivara-qvt/main.cc
@@ -0,0 +1,29 @@
+/*
+ * main.cc
+ *
+ *  Created on: 19 de set de 2018
+ *      Author: gian
+ *
+ *  This file is part of capivara-qvt.
+ *  
+ *  capivara-qvt is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  capivara-qvt is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with capivara-qvt.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+int main(int argc, char **argv) {
+	return 0;
+}
+
+
+
