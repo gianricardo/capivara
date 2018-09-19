fce1410a853e6d85a31e15232d06d0cbddc2c19e
gian <gian.ricardo@gmail.com>
2018-09-19 18:48:30 -0300

    lets go

---
+++ capivara-qvt/inc/config.h
@@ -0,0 +1,30 @@
+/*
+ * config.h
+ *
+ *  Created on: 19 de set de 2018
+ *      Author: gian
+ *
+ *  This file is part of capivara-qvt.
+ *  
+ *  capivara-qvt is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  capivara-qvt is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with capivara-qvt.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef INC_CONFIG_H_
+#define INC_CONFIG_H_
+
+
+
+
+
+#endif /* INC_CONFIG_H_ */
